1f856caf898d9835473b83e38d71c6c144325d30
l2m2 <l2m2lq@gmail.com>
2020-08-26 14:45:19 +0800

    upload

---
+++ exercise-reader/exercise-reader.go
@@ -0,0 +1,18 @@
+package main
+
+import "golang.org/x/tour/reader"
+
+type MyReader struct{}
+
+// TODO: 给 MyReader 添加一个 Read([]byte) (int, error) 方法
+
+// https://www.reddit.com/r/golang/comments/2pa8mx/exercise_readers/
+
+func (r MyReader) Read(b []byte) (int, error) {
+	b[0] = 'A'
+	return 1, nil
+}
+
+func main() {
+	reader.Validate(MyReader{})
+}
---
+++ reader/go.mod
@@ -0,0 +1,3 @@
+module l2m2.top/tour/reader
+
+go 1.15
